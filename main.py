c5088427329993f7bce01af633be7110cf773315
Варов Михаил <8605495@mail.ru>
2022-07-30 15:01:52 +0300

    fix first message, end of file

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from config import ACCESS_TOKEN, tok
 from vkinder_class import VKinder
 import psycopg2
 
+# from funk import replay, insert_into_black_list, insert_into_like_list, next_person
+
 conn = psycopg2.connect(dbname='vkkinder', user='postgres', password='ENot3112', host='localhost')
 
 keyboard = VkKeyboard(inline=True)
@@ -22,28 +24,50 @@ vk = session.get_api()
 longpoll = VkLongPoll(session)  # сообщаем что хотим исп именно подключение через VkLongPoll
 
 
-def replay(id, text):  # Создадим функцию для ответа на сообщения в лс группы
+def replay(id, text):  # Ответ плюс клавиатура
     session.method('messages.send', {'user_id': id,
                                      'message': text,
                                      'random_id': 0,
                                      'attachment': ','.join(attachments),
                                      'keyboard': keyboard.get_keyboard()
                                      })
+
+
+def replay_without_keyboard(id, text):  # Ответ без клавиатуры
+    session.method('messages.send', {'user_id': id,
+                                     'message': text,
+                                     'random_id': 0,
+                                     'attachment': ','.join(attachments)})
+
+
 def insert_into_black_list(user_name, link, id):
-    cursor.execute('''INSERT INTO black_list (user_name, link, id) 
+    cursor.execute('''INSERT INTO black_list (user_name, link, id)
                     VALUES (%s, %s, %s);''', (user_name, link, id))
     conn.commit()
+
 
 def insert_into_like_list(user_name, link, id):
-    cursor.execute('''INSERT INTO like_list (user_name, link, id) 
+    cursor.execute('''INSERT INTO like_list (user_name, link, id)
                     VALUES (%s, %s, %s);''', (user_name, link, id))
     conn.commit()
 
-def next_person(): # следующий человек(меняется только после start/next)
-    user_data = next(items)
-    first_name, last_name, link = user_data[0].replace('\n', '').split(' ')
-    user_name = first_name + ' ' + last_name
+
+def next_person(items):  # следующий человек(меняется только после start/next)
+    try:
+        user_data = next(items)
+        first_name, last_name, link = user_data[0].replace('\n', '').split(' ')
+        user_name = first_name + ' ' + last_name
+    except StopIteration:
+        print('КОНЕЦ')
+        user1 = VKinder(tok, gender, city, min_age, max_age, 5)
+        my_list = user1.search()
+        items = iter(my_list)
+        user_data = next(items)
+        first_name, last_name, link = user_data[0].replace('\n', '').split(' ')
+        user_name = first_name + ' ' + last_name
+        return user_data, user_name, link
     return user_data, user_name, link
+
 
 for event in longpoll.listen():  # Слушаем longpoll(Сообщения)
     with conn.cursor() as cursor:
@@ -53,70 +77,61 @@ for event in longpoll.listen():  # Слушаем longpoll(Сообщения)
             id = event.user_id
             cursor.execute(f'select id from users;')
             id_list = [i[0] for i in cursor.fetchall()]
-            print(id_list)
+            print('Это id_list\n', id_list)
             if id not in id_list:
                 cursor.execute('''INSERT INTO users (id) VALUES (%s);''', (id,))
                 conn.commit()
 
             if message == 'привет':
-                replay(id, 'Привет, укажите через запятую интересующий вас пол(м/ж),'
-                           ' город, минимальный возраст, максимальный возраст')
-            elif len(message.split(',')) == 4:
-                replay(id, 'Сейчас поищу')
-                replay(id, 'start - начать\n'
-                           'next - следующий человек\n'
-                           'like - добавить в избранное\n'
-                           'stop - остановить поиск\n'
-                           'list - показать список избранных\n'
-                           'Введите команду: ')
-                gender, city, min_age, max_age = [i.strip() for i in message.split(',')]
+                replay_without_keyboard(id, 'Укажите пол/город/минимальный возраст/максимальный возраст\n'
+                                            'Должно получиться вот так:\nм/Санкт-Петербург/30/40')
+            elif len(message.split('/')) == 4:
+                replay_without_keyboard(id, 'Сейчас поищу')
 
-                user1 = VKinder(tok, gender, city, min_age, max_age)
+                gender, city, min_age, max_age = [i.strip() for i in message.split('/')]
+                user1 = VKinder(tok, gender, city, min_age, max_age, 5)
                 my_list = user1.search()
-                items = iter(my_list)
-                print(items)
-
-            #elif message in ('start', 'next', 'like', 'stop', 'list'):
-                # user_data, user_name, link = next_person()
-                # print(user_data)
-                # user_data = next(items)
-                # first_name, last_name, link = user_data[0].replace('\n', '').split(' ')
-                # user_name = first_name + ' ' + last_name
-
+                if not my_list:
+                    replay_without_keyboard(id, 'Неверный запрос, попробуйте еще раз')
+                else:
+                    items = iter(my_list)
+                    print('Это items\n', items)
+                    print('Это my_list\n', my_list)
+                    replay(id, 'start - начать\n'
+                               'next - следующий человек\n'
+                               'like - добавить в избранное\n'
+                               'stop - остановить поиск\n'
+                               'list - показать список избранных\n')
             elif message == 'start' or message == 'next':
-                user_data, user_name, link = next_person()
+                user_data, user_name, link = next_person(items)  # СЛЕДУЮЩИЙ ЧЕЛОВЕК ИЗ СПИСКА
                 print(user_data)
                 with conn.cursor() as cursor:
                     cursor.execute(f'SELECT link FROM black_list ')
                     link_list = cursor.fetchall()
-                    if link_list:      # если в таблице black_list есть ссылки(она не пустая)
+                    if link_list:  # если в таблице black_list есть ссылки(она не пустая)
                         cursor.execute('''SELECT link FROM black_list WHERE id = %s;''', (id,))
                         link_list = [i[0] for i in cursor.fetchall()]
                         print(f'{link_list = }')
-                        if link not in link_list:   # если ссылки нет в черном списке
+                        if link not in link_list:  # если ссылки нет в черном списке
                             if len(user_data[1]) != 0:
                                 attachments = user_data[1]
                                 replay(id, user_data[0])
                                 insert_into_black_list(user_name, link, id)
-                                # cursor.execute('''INSERT INTO black_list (user_name, link, id)
-                                #                 VALUES (%s, %s, %s);''', (user_name, link, id))
-                                # conn.commit()
+
                             else:
                                 replay(id, f'{user_data[0]}\nНа странице нет фото')
                                 insert_into_black_list(user_name, link, id)
-                        else:   # если ссылка есть в черном списке
+                        else:  # если ссылка есть в черном списке
                             while True:
-                                user_data = next(items)
-                                first_name, last_name, link = user_data[0].replace('\n', '').split(' ')
-                                user_name = first_name + ' ' + last_name
+                                user_data, user_name, link = next_person(items)  # СЛЕДУЮЩИЙ ЧЕЛОВЕК ИЗ СПИСКА
                                 if link not in link_list:
                                     if len(user_data[1]) != 0:
                                         insert_into_black_list(user_name, link, id)
                                         attachments = user_data[1]
                                         replay(id, user_data[0])
                                         break
-                            #continue
-                    else:    # если таблица black_list пустая
+
+                    else:  # если таблица black_list пустая
                         if len(user_data[1]) != 0:
                             attachments = user_data[1]
                             replay(id, user_data[0])
@@ -141,9 +156,8 @@ for event in longpoll.listen():  # Слушаем longpoll(Сообщения)
                         replay(id, el)
                 else:
                     replay(id, 'Ваш список пуст')
-
             elif message == 'stop':
                 replay(id, "Чтобы продолжить нажмите START")
-                #break
+                # break
             else:
-                replay(id, 'Меня к такому не готовили')
+                replay_without_keyboard(id, 'Меня к такому не готовили\n Лучше напишите привет.')
--- vkinder_class.py
+++ vkinder_class.py
@@ -1,8 +1,13 @@
 import requests
 from config import tok
 from pprint import pprint
+
+
 class VKinder:
-    def __init__(self, user_token, gender, city, min_age, max_age):
+    offset = 0
+
+    def __init__(self, user_token, gender, city, min_age, max_age, count):
+        self.count = count
         self.user_token = user_token
         self.params = {'access_token': self.user_token, 'v': '5.131'}
         self.base_url = 'https://api.vk.com/method/'
@@ -18,28 +23,32 @@ class VKinder:
                   'album_id': 'profile',
                   'extended': 1}
         res = requests.get(self.base_url + method, params={**params, **self.params}).json()
-        #pprint(res)
+        # pprint(res)
+
         for el in res['response']['items']:
             result[f"photo{el['owner_id']}_{el['id']}"] = el['likes']['count']
         user_photo = [photo for photo in sorted(result, key=result.get, reverse=True)[:3]]
-            #result[el['sizes'][-1]['url']] = el[f'likes']['count']
+        # result[el['sizes'][-1]['url']] = el[f'likes']['count']
         return user_photo
 
     def search(self):
-        if self.gender.lower() in ['мужской', 'м']:
+        if self.gender.lower() in ['мужской', 'мужчина', 'муж', 'м']:
             self.gender = 2
-        elif self.gender.lower() in ['женский', 'ж']:
+        elif self.gender.lower() in ['женский', 'женщина', 'жен', 'ж']:
             self.gender = 1
         method = 'users.search'
-        params = {'count': 10,
-                  'can_access_closed':True,
+        params = {'count': self.count,
+                  'offset': VKinder.offset,
+                  'can_access_closed': True,
                   'age_from': self.min_age,
                   'age_to': self.max_age,
                   'hometown': self.city,
                   'sex': self.gender,
                   'fields': 'home_town'}
+
         res = requests.get(self.base_url + method, params={**params, **self.params}).json()
-        pprint(res)
+        VKinder.offset += self.count
+        # pprint(res)
         l = []
         for el in res['response']['items']:
 
